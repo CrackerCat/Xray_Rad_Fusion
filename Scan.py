f3ae29b9f79b57b58ac3599395ad646aee5e6324
ox01024 <52460705+ox01024@users.noreply.github.com>
2020-11-29 08:24:38 +0800

    2020：11：29 修复上一版本 关于时间戳和关闭主程序却无法关闭xray的BUG

--- Scan.py
+++ Scan.py
@@ -1,18 +1,20 @@
 import time
 import subprocess
-#rad路径(建议使用相对路径)
-radPath="./Rad/rad.exe"
-#xray路径(建议使用相对路径)
-xrayPath="./Xray/xray_windows_amd64.exe"
+import os
+#rad名字（必须将此文件与rad程序放在同一目录）
+radName="rad_windows_amd64.exe"
+#xray名字(必须将此文件与rad程序放在同一目录)
+xrayName="xray_windows_amd64.exe"
 #xray代理地址
 xrayProxy="127.0.0.1:7777"
-def Scan(target,startxray):
-	cmd = [radPath,"-t",target,"-http-proxy",xrayProxy]
+def Scan(target):
+	cmd = [radName,"-t",target,"-http-proxy",xrayProxy]
 	rsp=subprocess.Popen(cmd,start_new_session=True)
 	rsp.communicate()
 with open("target.txt","r") as targats:
-	result=time.strftime("%Y-%m-%d/%H:%M:%S",time.localtime())
-	xrayShell = [xrayPath, "webscan", "--listen", xrayProxy, "--html-output", result]
-	startxray = subprocess.Popen(xrayShell)
+	outputPath=time.strftime("%Y%m%d%H%M%S",time.localtime())
+	xrayShell = "start cmd /k "+xrayName+" webscan"+" --listen "+xrayProxy+" --html-output "+outputPath+".html"
+	os.system(xrayShell)
 	while targat :=targats.readline().strip("\n"):
-		Scan(targat,startxray)
+		Scan(targat)
+
